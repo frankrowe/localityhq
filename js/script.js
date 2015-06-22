e7df0b9d4adbb6fcf83d6e6b9392b3acfa62cb54
Frank Rowe <fsrowe@gmail.com>
2015-06-22 10:31:28 -0400

    redesign

--- js/script.js
+++ js/script.js
@@ -1,30 +1,36 @@
-$(document).ready(function(){
-  $( window ).resize(placeLogo);
-  placeLogo();
-  makeMap();
-});
-
-function placeLogo() {
-  var bodyWidth = $('body').width();
-  var imgWidth = $('.logo img').width();
-  imgWidth = 320;
-  $('.logo').css('margin-left', (bodyWidth-imgWidth)/2);
-}
-
 function makeMap(){ 
   var map = new L.Map('map', {
     attributionControl: false,
     zoomControl: false
-  });
-  var us = new L.LatLng(34.813803317113155, -97.6904296875);
-
-  map.setView(us, 4);
-
-  //L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png').addTo(map);
-  //L.tileLayer('http://{s}.tiles.mapbox.com/v3/fsrw.map-8diawj50/{z}/{x}/{y}.png').addTo(map);
-    L.tileLayer('http://{s}.tiles.mapbox.com/v3/fsrw.map-ckjf0e4p/{z}/{x}/{y}.png').addTo(map);
-
-  map.on('moveend', function(){
-    console.log(map.getCenter(), map.getZoom());
   })
+
+  var us = new L.LatLng(40, -140)
+
+  map.setView([0,0], 5)
+
+  L.tileLayer('http://{s}.tiles.mapbox.com/v3/fsrw.mg9npd88/{z}/{x}/{y}.png', {
+    detectRetina: true
+  }).addTo(map)
+
+  setTimeout(function() {
+    map.setView(randomLatLng(), 5, { pan: { animate: true, duration: 2, easeLinearity: .1} } )
+    setInterval(function() {
+      map.setView(randomLatLng(), 5, { pan: { animate: true, duration: 2, easeLinearity: .1} } )
+    }, 10000)
+  }, 3000)
 }
+
+function randomLatLng() {
+  var lat = Math.floor(Math.random() * 181) - 90
+  var lng = Math.floor(Math.random() * 361) - 180
+  return [lat, lng]
+}
+
+function randomZoom() {
+  return Math.floor(Math.random() * 18) + 1
+}
+
+$(document).ready(function(){
+  makeMap()
+})
+
